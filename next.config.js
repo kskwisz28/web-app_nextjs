be3c83265906e5d2e209858421a2f958cc19a3e2
kskwisz28 <kskwisz2853@gmail.com>
2023-02-19 16:49:13 +0600

    setup sentry

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+const {withSentryConfig} = require("@sentry/nextjs");
 const {i18n} = require('./next-i18next.config')
 
 /** @type {import('next').NextConfig} */
@@ -6,7 +7,14 @@ const nextConfig = {
   i18n,
   images: {
     domains: ['cdn.sanity.io'],
+  },
+  sentry: {
+    hideSourceMaps: true,
   }
 }
 
-module.exports = nextConfig
+const sentryWebpackPluginOptions = {
+  silent: true, // Suppresses all logs
+};
+
+module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
