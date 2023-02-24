f6b767b0d876ef1d1c5a8856c20729108dba729c
kskwisz28 <kskwisz2853@gmail.com>
2023-02-24 17:43:31 +0600

    add facebook pixel

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,9 +6,12 @@ import {isHomepage} from "@/helpers/general";
 import {IntercomProvider} from "react-use-intercom";
 import Head from "next/head";
 
+import * as fbq from '@/lib/fpixel'
+
 import 'vanilla-cookieconsent/dist/cookieconsent'
 import 'vanilla-cookieconsent/dist/cookieconsent.css'
 import {setCookieConsent} from "@/lib/cookieConsent";
+import Script from "next/script";
 
 export const AlternateLinksContext = createContext([])
 
@@ -45,6 +48,22 @@ function App({Component, pageProps}) {
     setCookieConsent()
   }, [])
 
+
+  const router = useRouter()
+  useEffect(() => {
+    // This pageview only triggers the first time (it's important for Pixel to have real information)
+    fbq.pageview()
+
+    const handleRouteChange = () => {
+      fbq.pageview()
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   const INTERCOM_APP_ID = 'pjlmfkmx'
 
   return (
@@ -56,6 +75,23 @@ function App({Component, pageProps}) {
           <Head>
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
           </Head>
+          <Script
+            id="fb-pixel"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', ${fbq.FB_PIXEL_ID});
+          `,
+            }}
+          />
           <Component {...pageProps} />
         </ThemeProvider>
       </IntercomProvider>
