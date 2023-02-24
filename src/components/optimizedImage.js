2327cca965590c67f44e100ef226d6ffc01048fe
kskwisz28 <kskwisz2853@gmail.com>
2023-02-24 20:03:18 +0600

    fix image size

--- src/components/optimizedImage.js
+++ src/components/optimizedImage.js
@@ -15,24 +15,34 @@ export default function OptimizedImage({
 
   const imageProps = useNextSanityImage(client, image);
   const styles = useMemo(() => {
+    let s = {}
     if (width) {
-      return {
+      s = {
         width: width,
         height: 'auto',
-        ...style,
       }
     } else if (height) {
-      return {
+      s = {
         width: 'auto',
         height: height,
-        ...style,
       }
     } else {
-      return {
+      s = {
         width: '100%',
         height: 'auto',
-        ...style,
       }
+    }
+
+    if (imageProps.width) {
+      s.maxWidth = imageProps.width
+    }
+    if (imageProps.height) {
+      s.maxHeight = imageProps.height
+    }
+
+    return {
+      ...s,
+      ...style
     }
   }, [width, height])
   return <Image
@@ -40,71 +50,4 @@ export default function OptimizedImage({
     style={styles}
     alt={alt}
   />
-  /*
-  let img = null
-  // TODO: Make images work
-  if (image && image.asset) {
-    let url = ''
-    if (!image.asset.url) {
-      const builder = imageUrlBuilder({
-        projectId: '7hja5omh',
-        dataset: 'production',
-      })
-      url = builder.image(image.asset).url()
-    } else {
-      url = image.asset.url
-    }
-    !noLazyLoad &&
-    (img = (
-      <React.Fragment>
-        {image && image.asset && image.asset.extension === 'svg' ? (
-          <Image
-            src={url}
-            alt={image.alt}
-            className={shadowImage ? 'infoImg' : ''}
-            layout="fill"
-            sx={{
-              objectFit: cover ? 'cover' : 'contain',
-              width: '100%',
-              borderRadius: rounded,
-              height: heightFull ? '100%' : '',
-              maxHeight: imgMaxHeight,
-              ...sx
-            }}
-          />
-        ) : (
-          <Image
-            alt={image.alt}
-            src={url}
-            className={shadowImage ? 'shadowImg' : ''}
-            fill
-            style={{
-              objectFit: cover ? 'cover' : 'contain',
-            }}
-          />
-        )}
-      </React.Fragment>
-    ))
-    noLazyLoad &&
-    (img = (
-      <Image
-        src={image?.asset?.url}
-        alt={image.alt}
-        className={shadowImage ? 'infoImg' : ''}
-        layout="fill"
-        sx={{
-          objectFit: cover ? 'cover' : 'contain',
-          width: '100%',
-          borderRadius: rounded,
-          height: heightFull ? '100%' : '',
-          maxHeight: imgMaxHeight,
-          // filter: 'grayscale(1)',
-          // ':hover': { filter: 'none' },
-        }}
-      />
-    ))
-  }
-  return img
-
-   */
 }
