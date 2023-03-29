4687dfa2db2fd0f4670e5cfef085caa5c93457a7
quickbutik-azddin <69296989+quickbutik-azddin@users.noreply.github.com>
2023-03-29 12:53:54 +0200

    rr

--- src/components/GA4Head.js
+++ src/components/GA4Head.js
@@ -3,18 +3,7 @@ const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA4_SV_ID;
 
 const GA4Head = () => (
     <Head>
-        {/* Global Site Tag (gtag.js) - Google Analytics 4 */}
         <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}></script>
-        <script
-            dangerouslySetInnerHTML={{
-                __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${GA_MEASUREMENT_ID}');
-        `,
-            }}
-        />
     </Head>
 );
 
