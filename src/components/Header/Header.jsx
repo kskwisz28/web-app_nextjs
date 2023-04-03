ff775665143930620f84f65b75c2963b0a0bc0c9
kskwisz28 <kskwisz2853@gmail.com>
2023-04-03 20:10:33 +0600

    Fix links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -125,20 +125,16 @@ export default function Header({
           flexGrow: '1',
           flexBasis: '0',
         }}>
-          <NextLink href={siteSettings.headerSecondButtonUrl[locale]} passHref legacyBehavior>
-            <Link sx={{variant: 'buttons.naExtraSmall'}}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="29" height="21" viewBox="0 0 25 21" fill="none">
-                <path
-                  d="M24.5 4.5V16.5C24.5 19.0312 22.4844 21 20 21H16.625C15.9688 21 15.5 20.5312 15.5 19.875C15.5 19.2656 15.9688 18.75 16.625 18.75H20C21.2188 18.75 22.25 17.7656 22.25 16.5V4.5C22.25 3.28125 21.2188 2.25 20 2.25H16.625C15.9688 2.25 15.5 1.78125 15.5 1.125C15.5 0.515625 15.9688 0 16.625 0H20C22.4844 0 24.5 2.01562 24.5 4.5ZM16.6719 9.75L10.6719 3.375C10.25 2.90625 9.54688 2.90625 9.07812 3.32812C8.60938 3.75 8.60938 4.45312 9.03125 4.92188L13.25 9.375H1.625C0.96875 9.375 0.5 9.89062 0.5 10.5C0.5 11.1562 0.96875 11.625 1.625 11.625H13.25L9.03125 16.125C8.60938 16.5938 8.60938 17.2969 9.07812 17.7188C9.3125 17.9062 9.59375 18 9.875 18C10.1562 18 10.4375 17.9062 10.6719 17.6719L16.6719 11.2969C17.0938 10.875 17.0938 10.1719 16.6719 9.75Z"
-                />
-              </svg>
-            </Link>
-          </NextLink>
-          <NextLink href={siteSettings.headerButtonUrl[locale]} passHref legacyBehavior>
-            <Link sx={{variant: 'buttons.blueMedium'}}>
-              {siteSettings.headerButtonText[locale]}
-            </Link>
-          </NextLink>
+          <Link href={siteSettings.headerSecondButtonUrl[locale]} sx={{variant: 'buttons.naExtraSmall'}}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="29" height="21" viewBox="0 0 25 21" fill="none">
+              <path
+                d="M24.5 4.5V16.5C24.5 19.0312 22.4844 21 20 21H16.625C15.9688 21 15.5 20.5312 15.5 19.875C15.5 19.2656 15.9688 18.75 16.625 18.75H20C21.2188 18.75 22.25 17.7656 22.25 16.5V4.5C22.25 3.28125 21.2188 2.25 20 2.25H16.625C15.9688 2.25 15.5 1.78125 15.5 1.125C15.5 0.515625 15.9688 0 16.625 0H20C22.4844 0 24.5 2.01562 24.5 4.5ZM16.6719 9.75L10.6719 3.375C10.25 2.90625 9.54688 2.90625 9.07812 3.32812C8.60938 3.75 8.60938 4.45312 9.03125 4.92188L13.25 9.375H1.625C0.96875 9.375 0.5 9.89062 0.5 10.5C0.5 11.1562 0.96875 11.625 1.625 11.625H13.25L9.03125 16.125C8.60938 16.5938 8.60938 17.2969 9.07812 17.7188C9.3125 17.9062 9.59375 18 9.875 18C10.1562 18 10.4375 17.9062 10.6719 17.6719L16.6719 11.2969C17.0938 10.875 17.0938 10.1719 16.6719 9.75Z"
+              />
+            </svg>
+          </Link>
+          <Link href={siteSettings.headerButtonUrl[locale]} sx={{variant: 'buttons.blueMedium'}}>
+            {siteSettings.headerButtonText[locale]}
+          </Link>
         </Box>
 
         <Box sx={{
@@ -183,22 +179,18 @@ export default function Header({
             flexDirection: 'column',
             gap: '10px',
           }}>
-            <NextLink href={siteSettings.headerSecondButtonUrl[locale]} passHref legacyBehavior>
-              <Link variant="white">
-                Sign in
-              </Link>
-            </NextLink>
-            <NextLink href={siteSettings.headerButtonUrl[locale]} passHref legacyBehavior>
-              <Link variant="blue">
-                {siteSettings.headerButtonText[locale]}
-                <svg sx={{ml: '12px'}} width="15" height="13" viewBox="0 0 15 13" fill="none"
-                     xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    d="M13.6875 7.71875L8.6875 12.7188C8.5 12.9062 8.25 13 8 13C7.71875 13 7.46875 12.9062 7.28125 12.7188C6.875 12.3438 6.875 11.6875 7.28125 11.3125L10.5625 8H1C0.4375 8 0 7.5625 0 7C0 6.46875 0.4375 6 1 6H10.5625L7.28125 2.71875C6.875 2.34375 6.875 1.6875 7.28125 1.3125C7.65625 0.90625 8.3125 0.90625 8.6875 1.3125L13.6875 6.3125C14.0938 6.6875 14.0938 7.34375 13.6875 7.71875Z"
-                    fill="#FAF8F7"/>
-                </svg>
-              </Link>
-            </NextLink>
+            <Link href={siteSettings.headerSecondButtonUrl[locale]} variant="white">
+              Sign in
+            </Link>
+            <Link href={siteSettings.headerButtonUrl[locale]} variant="blue">
+              {siteSettings.headerButtonText[locale]}
+              <svg sx={{ml: '12px'}} width="15" height="13" viewBox="0 0 15 13" fill="none"
+                   xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M13.6875 7.71875L8.6875 12.7188C8.5 12.9062 8.25 13 8 13C7.71875 13 7.46875 12.9062 7.28125 12.7188C6.875 12.3438 6.875 11.6875 7.28125 11.3125L10.5625 8H1C0.4375 8 0 7.5625 0 7C0 6.46875 0.4375 6 1 6H10.5625L7.28125 2.71875C6.875 2.34375 6.875 1.6875 7.28125 1.3125C7.65625 0.90625 8.3125 0.90625 8.6875 1.3125L13.6875 6.3125C14.0938 6.6875 14.0938 7.34375 13.6875 7.71875Z"
+                  fill="#FAF8F7"/>
+              </svg>
+            </Link>
           </Box>
         </Box>
 
